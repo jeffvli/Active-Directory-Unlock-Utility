37212b6db8c2dd74e047b7a89d41f95e17f241ce
Jeff <42182408+jeffvli@users.noreply.github.com>
2019-03-21 10:13:23 -0700

    Update AD-UnlockUtility.ps1

--- AD-UnlockUtility.ps1
+++ AD-UnlockUtility.ps1
@@ -6,41 +6,47 @@ function Unlock-ADUser {
         [Parameter(Mandatory=$true, Position=1)]
         [string]$LogPath
     )
-    $TimeStamp = Get-Date -Format "MM/dd/yyyy - HH:mm:ss"
-    $Credentials = Get-Credential -Message 'Enter AD admin credentials (User@Domain.com or Domain\Username)'
-    try {
-        $LockedUserSearch = Search-ADAccount `
-        -LockedOut `
-        -Credential $Credentials `
-        -Server $Server `
-        | Select-Object SamAccountName, LockedOut, DistinguishedName -ErrorAction Stop
-    }
-
-    catch {
-        if ($Credentials -eq $null -or $Credentials -eq '') {
-            break
-        }
-        else {
-            Write-Warning 'Please re-enter valid credentials.' | Tee-Object -FilePath $LogPath -Append
-            Unlock-ADUser
-        }
-    }
-
-    Write-Output "($TimeStamp) AD unlock utility has been started" | Tee-Object -FilePath $LogPath -Append
-
-    while ($true) {
+    
+    begin {
         $TimeStamp = Get-Date -Format "MM/dd/yyyy - HH:mm:ss"
-        if ($LockedUserSearch -eq $null -or $LockedUserSearch -eq "") {
-            Write-Output "($TimeStamp) No users are locked." | Tee-Object -FilePath $LogPath -Append
-            pause
+        $Credentials = Get-Credential -Message 'Enter AD admin credentials (User@Domain.com or Domain\Username)'
+    }
+    
+    process {
+        try {
+            $LockedUserSearch = Search-ADAccount `
+            -LockedOut `
+            -Credential $Credentials `
+            -Server $Server `
+            | Select-Object SamAccountName, LockedOut, DistinguishedName -ErrorAction Stop
         }
- 
-        else {
-            $LockedUserSelection = @($LockedUserSearch.SamAccountName)
-            foreach ($User in $LockedUserSelection) {
-                Unlock-ADAccount -Identity $User -Credential $Credentials -Server $ADServer
-                Write-Output "($TimeStamp) $User has been unlocked." | Tee-Object -FilePath $LogPath -Append
+
+        catch {
+            if ($Credentials -eq $null -or $Credentials -eq '') {
+                break
+            }
+            else {
+                Write-Warning 'Please re-enter valid credentials.' | Tee-Object -FilePath $LogPath -Append
+                Unlock-ADUser
+            }
+        }
+
+        Write-Output "($TimeStamp) AD unlock utility has been started" | Tee-Object -FilePath $LogPath -Append
+
+        while ($true) {
+            $TimeStamp = Get-Date -Format "MM/dd/yyyy - HH:mm:ss"
+            if ($LockedUserSearch -eq $null -or $LockedUserSearch -eq "") {
+                Write-Output "($TimeStamp) No users are locked." | Tee-Object -FilePath $LogPath -Append
                 pause
+            }
+
+            else {
+                $LockedUserSelection = @($LockedUserSearch.SamAccountName)
+                foreach ($User in $LockedUserSelection) {
+                    Unlock-ADAccount -Identity $User -Credential $Credentials -Server $ADServer
+                    Write-Output "($TimeStamp) $User has been unlocked." | Tee-Object -FilePath $LogPath -Append
+                    pause
+                }
             }
         }
     }
